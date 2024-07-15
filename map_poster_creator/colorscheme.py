7712654054b6c65fe7855ddaf48e21d222d0097c
Ivan Alsina <50876898+ialsina@users.noreply.github.com>
2024-07-15 13:17:23 +0200

    Refactor: Add classes `Color` and `ColorScheme`

--- map_poster_creator/colorscheme.py
+++ map_poster_creator/colorscheme.py
@@ -1,144 +1,131 @@
+from dataclasses import dataclass, field, asdict
 import json
 import logging
-import os
-import re
 from pathlib import Path
-from typing import Union, Tuple, List, Dict
+from typing import Union, Tuple, Sequence, Mapping, Optional
+
+from colour import Color
+from matplotlib import colors
 
 from map_poster_creator.config import paths
 
 logger = logging.getLogger(__name__)
+_CONFIG_COLORSCHEME_PATH = paths.colors
 
-base_color_scheme = {
-    "META_INFO": {
-        "config_version": 1,
-        "defaults": {
-            "facecolor": [0, 0, 0],
-            "water": "#383d52",
-            "greens": "#354038",
-            "roads": "#ffffff",
-        }
-    },
-    "black": {
-        "facecolor": [0, 0, 0],
-        "water": "#383d52",
-        "greens": "#354038",
-        "roads": "#ffffff",
-    },
-    "white": {
-        "facecolor": [1, 1, 1],
-        "water": "#bdddff",
-        "greens": "#d4ffe1",
-        "roads": "#000000",
-    },
-    "red": {
-        "facecolor": [0.4, 0.12, 0.12],
-        "water": "#754444",
-        "greens": "#b36969",
-        "roads": "#ffffff",
-    },
-    "coral": {
-        "facecolor": [0.67, 0.2, 0.18],
-        "water": "#ffffff",
-        "greens": "#b36969",
-        "roads": "#ffffff",
-    },
+@dataclass
+class ColorScheme:
+    facecolor: Color = field(default_factory=lambda: Color("#ffffff"))
+    water: Color = field(default_factory=lambda: Color("#ffffff"))
+    greens: Color = field(default_factory=lambda: Color("#ffffff"))
+    roads: Color = field(default_factory=lambda: Color("#ffffff"))
+
+    @staticmethod
+    def _make_color(arg=None):
+        _type_error = TypeError(
+            f"Bad types in Color construction with argument {arg}."
+        )
+        if isinstance(arg, Color):
+            return arg
+        if arg is None:
+            return Color("#000000")
+        if isinstance(arg, str):
+            return Color(arg)
+        if isinstance(arg, (list, tuple)):
+            if not all(isinstance(element, (int, float)) for element in arg):
+                raise _type_error
+            if not len(arg) == 3:
+                raise _type_error
+            return Color(rgb=arg)
+        raise TypeError(
+            f"Error while constructing Color, wrong type ()"
+        )
+
+    def __init__(self, facecolor, water=None, greens=None, roads=None):
+        self.facecolor = self._make_color(facecolor)
+        self.water = self._make_color(water or facecolor)
+        self.greens = self._make_color(greens or facecolor)
+        self.roads = self._make_color(roads or facecolor)
+
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, Color):
+            return o.hex
+        elif isinstance(o, ColorScheme):
+            return {"__ColorScheme__": asdict(o)}
+        return super().default(o)
+
+def object_hook(obj):
+    if isinstance(obj, str):
+        if obj.startswith("#"):
+            return Color(obj)
+    if isinstance(obj, dict):
+        if "__ColorScheme__" in obj:
+            return ColorScheme(**obj["__ColorScheme__"])
+    return obj
+
+
+_DEFAULT_SCHEMES = {
+    "black": ColorScheme(
+        (0, 0, 0),
+        "#383d52",
+        "#354038",
+        "#ffffff",
+    ),
+    "white": ColorScheme(
+        (1, 1, 1),
+        "#bdddff",
+        "#d4ffe1",
+        "#000000",
+    ),
+    "red": ColorScheme(
+        (0.4, 0.12, 0.12),
+        "#754444",
+        "#b36969",
+        "#ffffff",
+    ),
+    "coral": ColorScheme(
+        (0.67, 0.2, 0.18),
+        "#ffffff",
+        "#b36969",
+        "#ffffff",
+    ),
 }
 
+def _save_colorschemes(
+    schemes: Mapping[str, ColorScheme],
+) -> None:
+    with open(_CONFIG_COLORSCHEME_PATH, "w") as cf:
+        json.dump(schemes, cf, cls=JSONEncoder)
+    logger.info(f"Save user colors config: {_CONFIG_COLORSCHEME_PATH}")
 
-def is_hex_color(hex_color: str) -> bool:
-    match = re.search(r'^#(?:[0-9a-fA-F]{3}){1,2}$', hex_color)
-    return True if match else False
-
-
-def hex_to_faceacolor(hex_color: str) -> Tuple[float]:
-    if not is_hex_color(hex_color):
-        raise ValueError(f"{hex_color} is a not hex color. Expected eq. '#ffffff'")
-
-    color = hex_color.strip("#")
-    return tuple(1 / 255 * int(color[i:i+2], 16) for i in (0, 2, 4))  # magic asshole-code
-
-
-def get_color_schemes() -> Dict[str, Dict[str, Union[List[float], Tuple[float]]]]:
-    fpath = paths.colors
-    ensure_user_colors_or_create(fpath)
-    update_user_colors_if_need(fpath)
-    with open(fpath, "r") as conf:
-        color_scheme = json.load(conf)
-
-    return color_scheme
-
-
-def ensure_user_colors_or_create(config_path: Path) -> None:
-    if config_path.is_file():
+def _ensure_colorscheme_config_file() -> None:
+    if _CONFIG_COLORSCHEME_PATH.is_file():
         return
-    if not config_path.parent.is_dir():
-        os.makedirs(config_path.parent)
-
+    _CONFIG_COLORSCHEME_PATH.parent.mkdir(parents=True, exist_ok=True)
     logger.info(f"User colors config not found!")
-    save_user_color_schemes(config_path=config_path, color_schemes=base_color_scheme)
+    _save_colorschemes(schemes=_DEFAULT_SCHEMES)
 
+def get_colorschemes() -> dict[str, ColorScheme]:
+    with open(_CONFIG_COLORSCHEME_PATH, "r", encoding="utf-8") as cf:
+        return json.load(cf, object_hook=object_hook)
 
-def update_user_colors_if_need(config_path: Path):
-    # TODO: Add logic for update config version
-    pass
+def get_available_colorschemes():
+    return list(get_colorschemes().keys())
 
+def get_colorscheme(name):
+    return get_colorschemes()[name]
 
-def save_user_color_schemes(
-        config_path: Path,
-        color_schemes: Dict[str, Dict[str, Union[List[float], Tuple[float]]]],
-) -> None:
-    with open(config_path, "w") as conf:
-        json.dump(color_schemes, conf)
-    logger.info(f"Save user colors config: {config_path}")
+def add_colorscheme(name: str, colorscheme: ColorScheme):
+    colorschemes = get_colorschemes()
+    colorschemes.update({name: colorscheme})
+    _save_colorschemes(colorschemes)
 
+def remove_colorscheme(name: str):
+    colorschemes = get_colorschemes()
+    del colorschemes[name]
+    _save_colorschemes(colorschemes)
 
-def compose_user_color_scheme(
-        name: str,
-        facecolor: Union[Tuple[Union[float, int]], List[Union[float, int]], str],
-        water: str,
-        greens: str,
-        roads: str
-) -> Dict[str, Dict[str, Union[List[float], Tuple[float]]]]:
-    if isinstance(facecolor, str):
-        if not is_hex_color(facecolor):
-            raise ValueError(f"{facecolor} is a not hex color. Expected eq. '#ffffff'")
-        facecolor = hex_to_faceacolor(facecolor)
-
-    if not all([is_hex_color(val) for val in [water, greens, roads]]):
-        raise ValueError(f"Hex color validation error. Expected eq. '#ffffff'")
-
-    new_scheme = {
-        name: {
-            "facecolor": facecolor,
-            "water": water,
-            "greens": greens,
-            "roads": roads,
-        }
-    }
-    return new_scheme
-
-
-def add_user_color_scheme(
-        name: str,
-        facecolor: Union[Tuple[Union[float, int]], List[Union[float, int]], str],
-        water: str,
-        greens: str,
-        roads: str
-) -> None:
-    current_color_schemes = get_color_schemes()
-    if name in current_color_schemes.keys():
-        logger.warning(f"Color scheme {name} exists. Will be rewrite.")
-
-    new_scheme = compose_user_color_scheme(
-        name=name,
-        facecolor=facecolor,
-        water=water,
-        greens=greens,
-        roads=roads,
-    )
-
-    fpath = paths.colors
-    current_color_schemes.update(new_scheme)
-    save_user_color_schemes(config_path=fpath, color_schemes=current_color_schemes)
+_ensure_colorscheme_config_file()
 
