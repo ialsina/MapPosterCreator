829b2cc0d934f582cdfb023c1a0573e8aa9da99a
Ivan Alsina <50876898+ialsina@users.noreply.github.com>
2024-07-16 00:54:32 +0200

    Update: Replace and add data paths in config file

--- scripts/get_city_data.py
+++ scripts/get_city_data.py
@@ -9,9 +9,9 @@ API_URL = "https://api.github.com/repos/datasets/world-cities/commits?path=data/
 DATA_URL = "https://raw.githubusercontent.com/datasets/world-cities/master/data/world-cities.csv"
 
 def get_github_commit_hash():
-    if not paths.cities_hash.exists():
+    if not paths.cities_gh_datasets.exists():
         return ""
-    with open(paths.cities_hash, "r", encoding="utf-8") as hf:
+    with open(paths.cities_gh_datasets_hash, "r", encoding="utf-8") as hf:
         return hf.read()
 
 def get_saved_commit_hash():
@@ -20,13 +20,13 @@ def get_saved_commit_hash():
     return commits[0]['sha']
 
 def fetch_data():
-    paths.cities.parent.mkdir(parents=True, exist_ok=True)
-    wget.download(DATA_URL, out=str(paths.cities))
-    with open(paths.cities_hash, "w", encoding="utf-8") as hf:
+    paths.cities_gh_datasets.parent.mkdir(parents=True, exist_ok=True)
+    wget.download(DATA_URL, out=str(paths.cities_gh_datasets))
+    with open(paths.cities_gh_datasets_hash, "w", encoding="utf-8") as hf:
         hf.write(get_github_commit_hash())
 
 def read_cities_df():
-    return read_csv(paths.cities)
+    return read_csv(paths.cities_gh_datasets)
 
 
 if __name__ == "__main__":
