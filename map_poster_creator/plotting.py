2c5188d35867011b3373a435b81be63f597ea031
Ivan Alsina <50876898+ialsina@users.noreply.github.com>
2024-07-15 09:32:39 +0200

    Update: add customization in plotting.py

--- map_poster_creator/plotting.py
+++ map_poster_creator/plotting.py
@@ -1,7 +1,9 @@
 import math
+from typing import Tuple, Optional
 
 from geopandas import GeoDataFrame
 from matplotlib import pyplot as plt
+from matplotlib.axes import Axes
 
 from map_poster_creator.geojson import MapGeometry
 from map_poster_creator.logs import log_processing
@@ -26,49 +28,34 @@ def plot_and_save(
         color: dict,
         geometry: MapGeometry,
         path: str,
-        dpi: int = 300,
+        figsize: Optional[Tuple[float, float]] = (19, 19),
+        dpi: Optional[int] = 300,
 ) -> None:
-
-    ax = set_subplot(color)
-
+    plt.clf()
+    fig, ax = plt.subplots(figsize=figsize, facecolor=color['facecolor'])
     plot_water(ax, color, water)
-
     plot_greens(ax, color, greens)
-
     plot_roads(ax, color, roads)
-
-    aspect = 1 / math.cos(math.pi / 180 * geometry.center[0])
-    ax.set_aspect(aspect)
+    ax.set_aspect(
+        1 / math.cos(math.pi / 180 * geometry.center[0])
+    )
     ax.set_ylim((geometry.bottom, geometry.top))
     ax.set_xlim((geometry.left, geometry.right))
-    plt.axis('off')
-    save_image(dpi, path)
-
-
-@log_processing
-def save_image(dpi: int, path: str) -> None:
-    plt.savefig(path, bbox_inches='tight', dpi=dpi)
-
+    ax.set_axis_off()
+    fig.savefig(path, bbox_inches='tight', dpi=dpi)
 
 @log_processing
-def set_subplot(color: dict) -> plt.subplot:
-    plt.clf()
-    f, ax = plt.subplots(1, figsize=(19, 19), facecolor=color['facecolor'])
-    return ax
-
-
-@log_processing
-def plot_water(ax: plt.subplot, color: dict, water: GeoDataFrame) -> None:
+def plot_water(ax: Axes, color: dict, water: GeoDataFrame) -> None:
     water.plot(ax=ax, color=color['water'], linewidth=0.1)
 
 
 @log_processing
-def plot_greens(ax: plt.subplot, color: dict, greens: GeoDataFrame) -> None:
+def plot_greens(ax: Axes, color: dict, greens: GeoDataFrame) -> None:
     greens.plot(ax=ax, color=color['greens'], linewidth=0.1)
 
 
 @log_processing
-def plot_roads(ax: plt.subplot, color: dict, roads: GeoDataFrame) -> None:
+def plot_roads(ax: Axes, color: dict, roads: GeoDataFrame) -> None:
     roads.plot(
         ax=ax,
         color=color['roads'],
