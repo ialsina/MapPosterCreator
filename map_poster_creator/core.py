9fad96a8c7850ef8b46aaf832885e421648a6d3a
Ivan Alsina <50876898+ialsina@users.noreply.github.com>
2024-07-17 01:10:59 +0200

    Refactor: Minor changes in `core.py`

--- map_poster_creator/core.py
+++ map_poster_creator/core.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from typing import Tuple, Sequence, Mapping, Union
 from pathlib import Path
 
@@ -12,47 +13,37 @@ from map_poster_creator.plotting import plot_and_save
 logger = logging.getLogger(__name__)
 
 
-@log_processing
-def get_roads_data(shp_path: Path) -> GeoDataFrame:
-    roads = GeoDataFrame.from_file(shp_path / "gis_osm_roads_free_1.shp", encoding='utf-8')
-    return roads
+@dataclass(frozen=True)
+class shp_filename:
+    roads = "gis_osm_roads_free_1.shp"
+    water = "gis_osm_water_a_free_1.shp"
+    greens = "gis_osm_pois_a_free_1.shp"
 
 
 @log_processing
-def get_water_data(shp_path: Path) -> GeoDataFrame:
-    water = GeoDataFrame.from_file(shp_path / "gis_osm_water_a_free_1.shp", encoding='utf-8')
-    return water
-
-
-@log_processing
-def get_greens_data(shp_path: Path) -> GeoDataFrame:
-    greens = GeoDataFrame.from_file(shp_path / "gis_osm_pois_a_free_1.shp", encoding='utf-8')
-    return greens
-
-
-@log_processing
-def get_boundary_shape(geojson) -> Tuple[Polygon, MapGeometry]:
+def _get_boundary_shape(geojson) -> Tuple[Polygon, MapGeometry]:
     poly = get_polygon_from_geojson(geojson)
     geometry = get_map_geometry_from_poly(poly)
     return poly, geometry
 
+@log_processing
+def _preprocessing(poly: Polygon, gdf: GeoDataFrame) -> GeoDataFrame:
+    def poly_contains(g):
+        return poly.contains(g)
+    town = gdf.loc[
+        gdf['geometry'].apply(poly_contains)
+    ].copy()
+    return town
 
 @log_processing
-def preprocessing_roads(poly: Polygon, roads: GeoDataFrame) -> GeoDataFrame:
-    town = roads.loc[roads['geometry'].apply(lambda g: poly.contains(g))].copy()
+def _preprocessing_roads(poly: Polygon, gdf: GeoDataFrame) -> GeoDataFrame:
+    town = _preprocessing(poly=poly, gdf=gdf)
     town = town[~town.fclass.isin(['footway', "steps"])]
     town['speeds'] = [speed for speed in town['maxspeed']]
     return town
 
-
-@log_processing
-def preprocessing_other(poly: Polygon, dataframe: GeoDataFrame) -> GeoDataFrame:
-    town = dataframe.loc[dataframe['geometry'].apply(lambda g: poly.contains(g))].copy()
-    return town
-
-
 def create_poster(
-        base_shp_path: Path,
+        shp_dir: Path,
         geojson_path: Path,
         color: ColorScheme,
         layers: Sequence[str],
@@ -60,19 +51,27 @@ def create_poster(
         output_prefix: str,
         user_color_scheme: bool = False,
 ):
-    poly, geometry = get_boundary_shape(geojson=geojson_path)
-    roads = get_roads_data(base_shp_path)
-    water = get_water_data(base_shp_path)
-    greens = get_greens_data(base_shp_path)
-    roads_df = preprocessing_roads(poly=poly, roads=roads)
-    water_df = preprocessing_other(poly=poly, dataframe=water)
-    greens_df = preprocessing_other(poly=poly, dataframe=greens)
+    poly, geometry = _get_boundary_shape(geojson=geojson_path)
+    roads = _preprocessing_roads(
+        poly=poly,
+        gdf=GeoDataFrame.from_file(shp_dir / shp_filename.roads, encoding="utf-8")
+    )
+    water = _preprocessing(
+        poly=poly,
+        gdf=GeoDataFrame.from_file(shp_dir / shp_filename.water, encoding="utf-8")
+    )
+    greens = _preprocessing(
+        poly=poly,
+        gdf=GeoDataFrame.from_file(shp_dir / shp_filename.greens, encoding="utf-8")
+    )
+
     plot_and_save(
-        roads=roads_df,
-        water=water_df,
-        greens=greens_df,
+        roads=roads,
+        water=water,
+        greens=greens,
         geometry=geometry,
         path=f'{output_prefix}_{color}.png',
         dpi=1200,
         cscheme=color,
     )
+
